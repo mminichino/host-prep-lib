ce95fad80169158041d20faf44d170192768dfe9
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2023-09-06 16:42:16 -0500

    first commit

---
+++ pyhostprep/storage.py
@@ -0,0 +1,25 @@
+##
+##
+
+import json
+from pyhostprep.command import RunShellCommand, RCNotZero
+
+
+class StorageMgrError(Exception):
+    pass
+
+
+class StorageManager(object):
+
+    def __init__(self):
+        cmd = ["lsblk", "--json"]
+
+        try:
+            output = RunShellCommand().cmd_output(cmd, "/var/tmp")
+        except RCNotZero as err:
+            raise StorageMgrError(f"can not get disk info: {err}")
+
+        disk_data = json.loads(output)
+
+        for device in disk_data.get('blockdevices', []):
+            print(device['name'])
