cf907bc240a1bb2e9eeb146e897e36a4e70c53a8
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2023-09-06 16:55:46 -0500

    'Version 0.1.16'

--- pyhostprep/__init__.py
+++ pyhostprep/__init__.py
@@ -2,7 +2,7 @@ import os
 from pkg_resources import parse_version
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
-__version__ = "0.1.15"
+__version__ = "0.1.16"
 VERSION = parse_version(__version__)
 
 
