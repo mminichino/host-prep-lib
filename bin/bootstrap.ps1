63e0f671760e59b6af85363c67c8533d3d5b172f
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-02-14 12:47:34 -0600

    Version 1.0.1

--- bin/bootstrap.ps1
+++ bin/bootstrap.ps1
@@ -1,10 +1,17 @@
+#
+#
+echo "Installing Scoop package manager"
+iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
+
+echo "Installing base software packages"
+Stop-Transcript | out-null
+Start-Transcript -path $env:Temp\bootstrap_log.txt -append
 
 scoop bucket add versions
-
 scoop install python311 -g
-
 scoop install git openssl cmake make jq -g
-
 scoop bucket add java
-
 scoop install maven microsoft11-jdk -g
+
+Stop-Transcript
+echo "Done."
