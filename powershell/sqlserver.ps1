e27225160f88f040e01074d589cf47e488c0a49f
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-04-11 13:24:31 -0500

    Version 1.0.13

--- powershell/sqlserver.ps1
+++ powershell/sqlserver.ps1
@@ -8,7 +8,9 @@ function Create-ShortCut {
         [Parameter(Mandatory = $True, Position = 0)]
         [string] $SourceExe,
         [Parameter(Mandatory = $True, Position = 1)]
-        [string] $ShortcutName
+        [string] $ShortcutName,
+        [Parameter(Mandatory = $False, Position = 2)]
+        [string] $Arguments
     )
 
     $DestinationPath = "$HOME\Desktop\$ShortcutName.lnk"
@@ -18,12 +20,17 @@ function Create-ShortCut {
     $WshShell = New-Object -comObject WScript.Shell
     $Shortcut = $WshShell.CreateShortcut($DestinationPath)
     $Shortcut.TargetPath = $SourceExe
+    if ($Arguments) {
+        $Shortcut.Arguments = $Arguments
+    }
     $Shortcut.Save()
 }
 
-curl -OLs https://download.microsoft.com/download/c/c/9/cc9c6797-383c-4b24-8920-dc057c1de9d3/SQL2022-SSEI-Dev.exe --output-dir C:\Temp
-curl -OLs https://download.microsoft.com/download/b/9/7/b97061b9-9b9c-4bc7-86de-22b262c016d1/SSMS-Setup-ENU.exe --output-dir C:\Temp
-curl -OLs https://raw.githubusercontent.com/couchbaselabs/host-prep-lib/main/powershell/SQLServerInstall.ini --output-dir C:\Temp
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
+(New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/c/c/9/cc9c6797-383c-4b24-8920-dc057c1de9d3/SQL2022-SSEI-Dev.exe", "C:\Temp\SQL2022-SSEI-Dev.exe")
+(New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/b/9/7/b97061b9-9b9c-4bc7-86de-22b262c016d1/SSMS-Setup-ENU.exe", "C:\Temp\SSMS-Setup-ENU.exe")
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/couchbaselabs/host-prep-lib/main/powershell/SQLServerInstall.ini", "C:\Temp\SQLServerInstall.ini")
 
 Get-Disk | Where-Object {$_.PartitionStyle -eq ‘Raw’} | Initialize-Disk -PartitionStyle GPT
 New-Partition -DiskNumber 2 -DriveLetter E -UseMaximumSize
@@ -31,8 +38,11 @@ Format-Volume -DriveLetter E -FileSystem NTFS
 
 C:\Temp\SQL2022-SSEI-Dev.exe /QUIET /ACTION=Download /MEDIAPATH=C:\Temp
 
-#C:\Temp\SQLServer2022-DEV-x64-ENU.exe /x:C:\Temp\SQLServer /ConfigurationFile=C\Temp\SQLServerInstall.ini
-#C:\Temp\SSMS-Setup-ENU.exe /Install /Passive /SSMSInstallRoot="C:\Program Files (x86)\Microsoft SQL Server Management Studio 20"
+C:\Temp\SQLServer2022-DEV-x64-ENU.exe /x:C:\Temp\SQLServer\ /q
+C:\Temp\SQLServer\SETUP.EXE /ConfigurationFile=C:\Temp\SQLServerInstall.ini
+C:\Temp\SSMS-Setup-ENU.exe /Install /Passive /SSMSInstallRoot="C:\Program Files (x86)\Microsoft SQL Server Management Studio 20"
 
-#Create-ShortCut "C:\Windows\SysWOW64\mmc.exe /32 C:\Windows\SysWOW64\SQLServerManager16.msc" "SQL Server Config"
-#Create-ShortCut "C:\Program Files (x86)\Microsoft SQL Server Management Studio 20\Common7\IDE\Ssms.exe" "SQL Server Management"
+Create-ShortCut "C:\Windows\SysWOW64\mmc.exe" "SQL Server Config" "/32 C:\Windows\SysWOW64\SQLServerManager16.msc"
+Create-ShortCut "C:\Program Files (x86)\Microsoft SQL Server Management Studio 20\Common7\IDE\Ssms.exe" "SQL Server Management"
+
+& "C:\Program Files\Microsoft SQL Server\160\Tools\Binn\osql" -E -Q "IF db_id('ycsb') IS NULL CREATE DATABASE [ycsb] ;"
